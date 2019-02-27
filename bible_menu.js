12808eed309a40b49d897acee77948742c5a4408
Samuel Sjödin <Samuel@Samuels-MacBook-Pro.local>
2019-02-27 18:19:31 +0100

    all books and chapters added

--- bible_menu.js
+++ bible_menu.js
@@ -50,14 +50,12 @@ plus.classList.add("fab");
     }
 
     function dragEnd(e) {
-
-      //navigate to selected chapter 
-      var elem = document.elementFromPoint(
-            e.changedTouches[0].pageX,
-            e.changedTouches[0].pageY,
-       );
-      var url = elem.url;
-      if(url){
+      var x = e.changedTouches?e.changedTouches[0].pageX:e.x;
+      var y = e.changedTouches?e.changedTouches[0].pageY:e.y;
+      //navigate to selected chapter
+      var elem = document.elementFromPoint(x,y);
+      if(elem && elem.url){
+          var url = elem.url;
           console.log(url);
           var iframe = document.getElementById("iframe");
           if(iframe){
@@ -66,6 +64,8 @@ plus.classList.add("fab");
               window.location = url;
           }
       }
+
+    //document.getElementById("chaptersContainer").classList.add("visible");
       
 
 
@@ -88,6 +88,14 @@ plus.classList.add("fab");
 
     function drag(e) {
       if (active) {
+
+        //TODO: if along the edge (x less than 50)
+        //then query for element ... (possibly hiding ball temporarily?)
+        //var elem = document.elementFromPoint(
+        //    e.changedTouches[0].pageX,
+        //    e.changedTouches[0].pageY,
+        //);
+        //then open tab associated with this section
         
         if(!e.targetTouches){
             e.preventDefault();
@@ -106,6 +114,7 @@ plus.classList.add("fab");
         yOffset = currentY;
         
         setTranslate(xOffset, yOffset, dragItem);
+        
       }
     }
 
@@ -118,21 +127,21 @@ plus.classList.add("fab");
 var menu = document.createElement("div");
 menu.classList.add("menu");
 content.appendChild(menu);
-menu.classList.add("visible");
+menu.setAttribute("id", "menu");
 
 content.appendChild(plus);
 
 var sectionsContainer = document.createElement("div");
+sectionsContainer.setAttribute("id", "sectionsContainer");
 sectionsContainer.classList.add("sections");
 menu.appendChild(sectionsContainer);
-sectionsContainer.classList.add("visible");
 var sectionsWidth = 50;
 sectionsContainer.style.width = sectionsWidth+"px";
 
 var chaptersContainer = document.createElement("div");
+chaptersContainer.setAttribute("id", "chaptersContainer");
 chaptersContainer.classList.add("chapters");
 menu.appendChild(chaptersContainer);
-//chaptersContainer.classList.add("visible");
 var chaptersWidth = window.innerWidth - sectionsWidth;
 chaptersContainer.style.width = chaptersWidth+"px";
 
@@ -140,42 +149,46 @@ var bright = true;
 
 function showChapters(e){
     chaptersContainer.classList.add("visible");
-    //chaptersContainer.removeChild(chaptersContainer.firstChild);
-    //chaptersContainer.appendChild(e.target.table);
+    try{
+        chaptersContainer.removeChild(chaptersContainer.firstChild);
+    }catch(e){}
+    chaptersContainer.appendChild(e.target.chaptersView);
 }
 
 function makeMenuSection(parent,cls,books,books_str){
     var section = document.createElement("div");
-    var table = document.createElement("table");
-    section.chaptersView = table;
-    chaptersContainer.appendChild(table);
+    var chaptersView = document.createElement("div");
+    section.chaptersView = chaptersView;
     //create chapters
     for (i = 0; i < books.length; i++) {
+        //show book title
         var book = books[i];
-        var row = table.insertRow(-1);
-        row.classList.add("booktitle");
-        //row.innerHTML = book.label;
-        for (i = 0; i < book.chapters; i++) {
+        var bookTitle = document.createElement("div");
+        bookTitle.classList.add("booktitle");
+        bookTitle.innerHTML = book.label;
+        chaptersView.appendChild(bookTitle);
+        var table = document.createElement("table");
+        chaptersView.appendChild(table);
+        
+        for (j = 0; j < book.chapters; j++) {
             //insert_chapter
-            if(i%5 == 0){
+            if(j%5 == 0){
                 var row = table.insertRow(-1);
             }
             
             var cell = row.insertCell(-1);
             //create div with link
-            cell.innerHTML = ""+(i+1);
+            cell.innerHTML = ""+(j+1);
             cell.classList.add("chapter");
             cell.book = book;
-            cell.chapter = i+1;
+            cell.chapter = j+1;
 
             cell.url = cell.book.urlStart + cell.chapter + cell.book.urlEnd;
-
+            
             //var chapter = document.createElement("div");
             //chapter.classList.add("chapter");
-            //chapter.innerHTML = ""+(i+1);
-            //chaptersContainer.appendChild(chapter);
+            //chapter.innerHTML = ""+(j+1);
         }
-    
     }
     
     section.classList.add(cls);
@@ -208,11 +221,217 @@ s1 = [
     }
 ];
 
+var s2 = [
+    {
+        "label":"Lukasevangeliet",
+        "short":"Luk",
+        "chapters":24,
+        "urlStart":"https://grundbibeln.se/luk-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Johannesevangeliet",
+        "short":"Joh",
+        "chapters":21,
+        "urlStart":"https://grundbibeln.se/joh-",
+        "urlEnd":"/"
+    }
+];
+
+var s3 = [
+    {
+        "label":"Apostlag\xE4rningarna",
+        "short":"Apg",
+        "chapters":28,
+        "urlStart":"https://grundbibeln.se/apg-",
+        "urlEnd":"/"
+    }
+];
+
+var s4 = [
+    {
+        "label":"Romarbrevet",
+        "short":"Rom",
+        "chapters":16,
+        "urlStart":"https://grundbibeln.se/rom-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"F\xF6rsta Korintierbrevet",
+        "short":"1 Kor",
+        "chapters":16,
+        "urlStart":"https://grundbibeln.se/1kor-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Andra Korintierbrevet",
+        "short":"2 Kor",
+        "chapters":13,
+        "urlStart":"https://grundbibeln.se/2kor-",
+        "urlEnd":"/"
+    }
+];
+
+var s5 = [
+    {
+        "label":"Galaterbrevet",
+        "short":"Gal",
+        "chapters":6,
+        "urlStart":"https://grundbibeln.se/gal-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Efesierbrevet",
+        "short":"Ef",
+        "chapters":6,
+        "urlStart":"https://grundbibeln.se/ef-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Filipperbrevet",
+        "short":"Fil",
+        "chapters":4,
+        "urlStart":"https://grundbibeln.se/fil-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Kolosserbrevet",
+        "short":"Kol",
+        "chapters":4,
+        "urlStart":"https://grundbibeln.se/kol-",
+        "urlEnd":"/"
+    }
+];
+
+var s6 = [
+    {
+        "label":"F\xF6rsta Thessalonikerbrevet",
+        "short":"1 Thess",
+        "chapters":5,
+        "urlStart":"https://grundbibeln.se/1thess-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Andra Thessalonikerbrevet",
+        "short":"2 Thess",
+        "chapters":3,
+        "urlStart":"https://grundbibeln.se/2thess-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"F\xF6rsta Timotheosbrevet",
+        "short":"1 Tim",
+        "chapters":6,
+        "urlStart":"https://grundbibeln.se/1tim-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Andra Timotheosbrevet",
+        "short":"2 Tim",
+        "chapters":4,
+        "urlStart":"https://grundbibeln.se/2tim-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Titusbrevet",
+        "short":"Tit",
+        "chapters":3,
+        "urlStart":"https://grundbibeln.se/tit-",
+        "urlEnd":"/"
+    }
+    ,{
+        "label":" Brevet Till Filemon",
+        "short":"Filem",
+        "chapters":1,
+        "urlStart":"https://grundbibeln.se/filem-",
+        "urlEnd":"/"
+    }
+];
+
+var s7 = [
+    {
+        "label":"Hebreerbrevet",
+        "short":"Heb",
+        "chapters":13,
+        "urlStart":"https://grundbibeln.se/1thess-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"Jakobsbrevet",
+        "short":"Jak",
+        "chapters":5,
+        "urlStart":"https://grundbibeln.se/jak-",
+        "urlEnd":"/"
+    },
+    {
+        "label":"F\xF6rsta Petrusbrevet",
+        "short":"1 Pet",
+        "chapters":5,
+        "urlStart":"https://grundbibeln.se/1pet-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Andra Petrusbrevet",
+        "short":"2 Pet",
+        "chapters":3,
+        "urlStart":"https://grundbibeln.se/2pet-",
+        "urlEnd":"/"
+    },
+    
+];
+var s8 = [
+    {
+        "label":"F\xF6rsta Johannesbrevet",
+        "short":"1 Joh",
+        "chapters":5,
+        "urlStart":"https://grundbibeln.se/1joh-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Andra Johannesbrevet",
+        "short":"2 Joh",
+        "chapters":1,
+        "urlStart":"https://grundbibeln.se/2joh-",
+        "urlEnd":"/"
+    },{
+        "label":" Tredje Johannesbrevet",
+        "short":"2 Joh",
+        "chapters":1,
+        "urlStart":"https://grundbibeln.se/3joh-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Judas brev",
+        "short":"Jud",
+        "chapters":1,
+        "urlStart":"https://grundbibeln.se/jud-",
+        "urlEnd":"/"
+    },
+    {
+        "label":" Uppenbarelseboken",
+        "short":"Upp",
+        "chapters":22,
+        "urlStart":"https://grundbibeln.se/upp-",
+        "urlEnd":"/"
+    }
+];
+
+
 var section1 = makeMenuSection(sectionsContainer,"section",s1,"<br>Matt<br>Mark");
-//var section2 = makeMenuSection(sectionsContainer,"section",s1,"<br>Luk<br>Joh");
-//var section3 = makeMenuSection(sectionsContainer,"section",s1,"<br><br>Apg");
-//var section4 = makeMenuSection(sectionsContainer,"section",s1,"<br>Rom<br>1-2 Kor");
-//var section5 = makeMenuSection(sectionsContainer,"section",s1,"Gal<br>Ef<br>Fil<br>Kol");
-//var section6 = makeMenuSection(sectionsContainer,"section",s1,"1-2 Thess<br>1-2 Tim<br>Tit<br>Filem");
-//var section7 = makeMenuSection(sectionsContainer,"section",s1,"Heb<br>Jak<br>1-2 Pet<br>1-3 Joh<br>Jud");
-//var section8 = makeMenuSection(sectionsContainer,"section",s1,"<br><br>Upp");
+var section2 = makeMenuSection(sectionsContainer,"section",s2,"<br>Luk<br>Joh");
+var section3 = makeMenuSection(sectionsContainer,"section",s3,"<br><br>Apg");
+var section4 = makeMenuSection(sectionsContainer,"section",s4,"<br>Rom<br>1-2 Kor");
+var section5 = makeMenuSection(sectionsContainer,"section",s5,"Gal<br>Ef<br>Fil<br>Kol");
+var section6 = makeMenuSection(sectionsContainer,"section",s6,"1-2 Thess<br>1-2 Tim<br>Tit<br>Filem");
+var section7 = makeMenuSection(sectionsContainer,"section",s7,"<br>Heb<br>Jak<br>1-2 Pet");
+var section8 = makeMenuSection(sectionsContainer,"section",s8,"<br>1-3 Joh<br>Jud<br>Upp");
+
+var showInitially = true;
+//
+
+if(showInitially){
+    menu.classList.add("visible");
+    chaptersContainer.classList.add("visible");
+    sectionsContainer.classList.add("visible");
+    chaptersContainer.appendChild(section4.chaptersView);
+}
